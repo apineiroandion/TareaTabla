50c90011eab1d4f41d347eeffc744c0bf388d3fb
Angel <apineiroandion@danielcastelao.org>
2024-04-15 10:48:32 +0200

    add padding

--- src/interfazUsuario/Ventana.java
+++ src/interfazUsuario/Ventana.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Ventana extends JFrame {
     public Ventana() {
         setTitle("Ventana");
-        setSize(300, 300);
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new GridBagLayout());
@@ -23,19 +23,23 @@ public class Ventana extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
+        Insets padding = new Insets(10, 10, 10, 10);
 
         //x=0,y=0 LabelName
         JLabel lblAlumno = new JLabel("Name");
         gbc.gridx = 0;
         gbc.gridy = 0;
+        gbc.insets = padding;
         panel.add(lblAlumno, gbc);
         //x=1,y=0 JTextName
         JTextField txtAlumno = new JTextField();
+        txtAlumno.setColumns(15);
         gbc.gridx = 1;
         gbc.gridy = 0;
         panel.add(txtAlumno, gbc);
         //x=2,y=0 JCombobox Curso
         JComboBox cbAlumno = new JComboBox();
+        cbAlumno.setPreferredSize(new Dimension(150, 30));
         gbc.gridx = 2;
         gbc.gridy = 0;
         panel.add(cbAlumno, gbc);
@@ -46,6 +50,7 @@ public class Ventana extends JFrame {
         panel.add(lblApelido, gbc);
         //x=1,y=1 JTextApelido
         JTextField txtApelido = new JTextField();
+        txtApelido.setColumns(15);
         gbc.gridx = 1;
         gbc.gridy = 1;
         panel.add(txtApelido, gbc);
